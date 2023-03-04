ea5e17510a94d40556a0df498fd3932915e66756
ammaarkhan <ammaarkhan03@gmail.com>
2023-03-03 17:19:57 -0800

    work on prepping data for training

--- training.py
+++ training.py
@@ -48,3 +48,33 @@ words = sorted(set(words)) # removes the duplicates
 pkl.dump(words, open('words.pkl', 'wb'))
 pkl.dump(classes, open('classes.pkl', 'wb'))
 
+
+# converting words to numbers
+# neural networks only work with numbers
+training = []
+output_empty = [0]*len(classes)
+
+# print(documents)
+print(words)
+for doc in documents:
+    bag = []
+    word_patterns = doc[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+        
+    # copy of output_empty
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+    training.append([bag, output_row])
+
+random.shuffle(training)
+training= np.array(training)
+
+# splitting data
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
+
+    
+    
