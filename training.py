948b6df4200f80ae852442edf632b29ab73b9c3f
ammaarkhan <ammaarkhan03@gmail.com>
2023-03-05 12:00:51 -0800

    comment training file for better understanding

--- training.py
+++ training.py
@@ -26,23 +26,24 @@ ignore_letters = ["?", "!", ",", "."]
 for intent in intents['intents']:
     for pattern in intent['patterns']:
         # separating words from patterns
-        word_list = nltk.word_tokenize(pattern) # returns an array of root words for each entry in the pattern, one by one
+        word_list = nltk.word_tokenize(pattern) # returns an array of words for each entry in the pattern, one by one for every pattern in patterns ex: ['hello']
         # print(word_list) # for testing
         words.extend(word_list) # adding the word_list output for each entry  of root words to words array
-        # print("this is the words: {}".format(words)) # for testing
+        # print("this is the words: {}".format(words)) # for testing, ex: ['hello', 'hi', 'hey', 'what', "'s", 'up', '?'] (after few iterations of loop)
         
         # linking patterns with respective tags and adding to documents list as a tuple
         documents.append(((word_list), intent['tag']))
-        # print(documents) # for testing
+        # print(documents) # for testing ex: [(['hello'], 'greeting')]\n[(['hello'], 'greeting'), (['hi'], 'greeting')]
         
         # adding tag to class list
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
-
+# print(classes) # for testing, ex: ['greeting', 'name', 'age']
 words = [lemmatizer.lemmatize(word) # lemmatizes the word - gives root word
     for word in words if word not in ignore_letters] 
 words = sorted(set(words)) # removes the duplicates
+# print(words) # for test, ex: ["'s", 'age', 'are', 'call'.......]
 
 # save the words and classes lists as binary files
 pkl.dump(words, open('words.pkl', 'wb'))
@@ -52,34 +53,37 @@ pkl.dump(classes, open('classes.pkl', 'wb'))
 # converting words to numbers
 # neural networks only work with numbers
 training = []
-output_empty = [0]*len(classes)
+output_empty = [0]*len(classes) # [0, 0, 0]
 
-# print(documents)
-# print(words)
-for doc in documents:
+for doc in documents: 
+    # print(doc) # ex: (['hello'], 'greeting')
     bag = []
-    word_patterns = doc[0]
+    word_patterns = doc[0] # ex: ['hello']
     word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
-    # print(word_patterns)
+    # print(word_patterns) # ^^ does nothing for the current intents we have
     for word in words:
         bag.append(1) if word in word_patterns else bag.append(0)
+        # print(bag)
         
     # copy of output_empty
-    output_row = list(output_empty)
-    output_row[classes.index(doc[1])] = 1 # finding index of the class to make it equal one for the training
-    training.append([bag, output_row])
+    tag_row = list(output_empty)
+    tag_row[classes.index(doc[1])] = 1 # finding index of the class to make it equal one for the training
+    training.append([bag, tag_row]) # adding array of 0s, 1s - 1 reps word,  array of 0s, 1s - 1 reps the tag
+    # print(training) # example: [[[0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [1, 0, 0]]]
 
 random.shuffle(training)
 training= np.array(training)
 # print(training)
 
-# splitting data
-train_x = list(training[:, 0])
-train_y = list(training[:, 1])
+# splitting data into x and y values: arrays of words and arrays of tags
+train_x = list(training[:, 0]) # ex: [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+# print(train_x)
+train_y = list(training[:, 1]) # ex: [1, 0, 0]
+# print(train_y)
 
 # model building
 model = Sequential()
-model.add(Dense(128, input_shape=(len(train_x[0]), ), activation='relu'))
+model.add(Dense(128, input_shape=(len(train_x[0])), activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
