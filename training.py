121760dd161ca06b2ae2436e83b4af35a059c8eb
ammaarkhan <ammaarkhan03@gmail.com>
2023-03-04 20:31:35 -0800

    complete first iteration of ML model

--- training.py
+++ training.py
@@ -55,26 +55,44 @@ training = []
 output_empty = [0]*len(classes)
 
 # print(documents)
-print(words)
+# print(words)
 for doc in documents:
     bag = []
     word_patterns = doc[0]
     word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
-    
+    # print(word_patterns)
     for word in words:
         bag.append(1) if word in word_patterns else bag.append(0)
         
     # copy of output_empty
     output_row = list(output_empty)
-    output_row[classes.index(doc[1])] = 1
+    output_row[classes.index(doc[1])] = 1 # finding index of the class to make it equal one for the training
     training.append([bag, output_row])
 
 random.shuffle(training)
 training= np.array(training)
+# print(training)
 
 # splitting data
 train_x = list(training[:, 0])
 train_y = list(training[:, 1])
 
-    
-    
+# model building
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]), ), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+# compiling model
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', 
+              optimizer=sgd, metrics=['accuracy'])
+hist = model.fit(np.array(train_x), np.array(train_y), 
+                 epochs=200, batch_size=5, verbose=1)
+
+# save the model 
+model.save("chatbotmodelv1.h5", hist)
+
+print("Training Complete!")
