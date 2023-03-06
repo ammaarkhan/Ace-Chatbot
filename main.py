ad8597c71e634a87579deb07635ce82e40c49aa5
ammaarkhan <ammaarkhan03@gmail.com>
2023-03-05 18:37:48 -0800

    finish commenting main.py file

--- main.py
+++ main.py
@@ -36,23 +36,24 @@ def bagofw(sent):
 
 def predict_class(sent):
     bow = bagofw(sent) # array of 0s and 1s, 1 representing the word is present
-    res = model.predict(np.array([bow]))[0] # predict and get the result
-    resss = model.predict(np.array([bow])) # ex: [[0.9838628  0.00768638 0.0084508 ]] (for input 'Hi')
-    ERROR_THRESHOLD = 0.25
-    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD] 
+    res = model.predict(np.array([bow]))[0] # predict and get the result, of type numpy.ndarray
+    # resss = model.predict(np.array([bow])) # ex: [[0.9838628 0.00768638 0.0084508 ]] (for input 'Hi')
+    ERROR_THRESHOLD = 0.75
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD] # this removes the values that are below the 0.25, ex: discards [0.00768638, 0.0084508]
     results.sort(key=lambda x: x[1], reverse=True) # sort from largest to smallest according to probability
-    return_list = []
-    for r in results: 
-        return_list.append({'intent': classes[r[0]],
-                            'probability': str(r[1])})
-    return return_list
+    print(results) # [[0, 0.9851791]] 
+    intents_list = []
+    for r in results: # r is [0, 0.9851791]
+        intents_list.append({'intent': classes[r[0]], # classes[0]
+                            'probability': str(r[1])}) # str(0.9851791)
+    return intents_list
     
 def get_response(intents_list, intents_json):
-    tag = intents_list[0]['intent']
-    list_of_intents = intents_json['intents']
+    tag = intents_list[0]['intent'] # gets the 'intent' value from the dictionary
+    list_of_intents = intents_json['intents'] # retrieving json file
     result = ""
-    for i in list_of_intents:
-        if i['tag'] == tag:
+    for i in list_of_intents: 
+        if i['tag'] == tag: # retrieving responses for that tag
             result = random.choice(i['responses'])
             break
     return result
@@ -63,7 +64,7 @@ while True:
     message = input("")
     if message == "end":
         break
-    ints = predict_class(message)
-    res = get_response(ints, intents)
-    print(res)
+    intents_list = predict_class(message) # [{'intent': 'greeting', 'probability': '0.9163127'}]
+    final_res = get_response(intents_list, intents) # randomly chosen response with same tag as prediction
+    print(final_res)
     
