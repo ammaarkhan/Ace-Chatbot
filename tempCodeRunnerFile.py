700c8d1d5f37b475bef46626f6e6d35a16f922fd
ammaarkhan <ammaarkhan03@gmail.com>
2023-03-05 19:09:00 -0800

    remove unneeded files

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-from tensorflow import keras
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout
-from keras.optimizers import SGD
-from nltk.stem import WordNetLemmatizer
