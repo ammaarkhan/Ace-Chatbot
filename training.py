8e1e32c8013f32c1ca5b84fed8e2a13b57b225f0
ammaarkhan <ammaarkhan03@gmail.com>
2023-03-17 23:26:41 -0700

    improve model

--- training.py
+++ training.py
@@ -83,10 +83,14 @@ train_y = list(training[:, 1]) # ex: [1, 0, 0]
 
 # model building
 model = Sequential()
+
+# model.add(Dense(256, input_shape=(len(train_x[0]), ), activation='relu')) #
+# model.add(Dropout(0.5)) #
+# model.add(Dense(128, activation='relu')) #
 model.add(Dense(128, input_shape=(len(train_x[0]), ), activation='relu'))
-model.add(Dropout(0.4))
+model.add(Dropout(0.5))
 model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.4))
+model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
 # compiling model
@@ -94,7 +98,7 @@ sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', 
               optimizer=sgd, metrics=['accuracy'])
 hist = model.fit(np.array(train_x), np.array(train_y), 
-                 epochs=70, batch_size=5, verbose=1)
+                 epochs=100, batch_size=5, verbose=1)
 
 # save the model 
 model.save("chatbotmodelv4.h5", hist)
