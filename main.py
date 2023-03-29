e47eed7e989f03347959198aaa4041b99a4eb5e9
ammaarkhan <ammaarkhan03@gmail.com>
2023-03-28 19:21:53 -0700

    add new major's courses and fix major not found error

--- main.py
+++ main.py
@@ -89,32 +89,38 @@ def ner_response(message):
     for ent in doc.ents:
         user.update({ent.label_: ent.text})
     
-    direct = {"computer science": ["cosc 121", "math 100", "math 101", "cosc 211", "cosc 221", "cosc 222", "math 221", "stat 230", "cosc 320", "cosc 304", "cosc 310" "cosc 341" "cosc 499", "phil 331"]}
-    optional = {"computer science": [["cosc 111", "cosc 123"], {"engl 109": ["2", "engl 112", "engl 113", "engl 114", "engl 150", "engl 151", "engl 153", "engl 154","engl 155", "engl 156", "engl 203", "corh 203", "corh 205", "apsc 176", "apsc 201"]}, ["phys 111", "phys 112"]]}
+    print("this is the course", user['COR'])
+    print("this is the major", user['MAJOR'])
+    
+    direct = {"computer science": ["cosc 121", "math 100", "math 101", "cosc 211", "cosc 221", "cosc 222", "math 221", "stat 230", "cosc 320", "cosc 304", "cosc 310" "cosc 341" "cosc 499", "phil 331"], "chemistry": ["math 100", "chem 201", "chem 220", "chem 203", "chem 203", "chem 204", "chem 211", "math 200"]}
+    optional = {"computer science": [["cosc 111", "cosc 123"], {"engl 109": ["2", "engl 112", "engl 113", "engl 114", "engl 150", "engl 151", "engl 153", "engl 154","engl 155", "engl 156", "engl 203", "corh 203", "corh 205", "apsc 176", "apsc 201"]}, ["phys 111", "phys 112"]], "chemistry": [["chem 111", "chem 121"], ["chem 113", "chem 123"], ["math 101", "math 103"], {"engl 109": ["2", "engl 112", "engl 113", "engl 114", "engl 150", "engl 151", "engl 153", "engl 154","engl 155", "engl 156", "corh 203"]}, ["phys 111", "phys 112"], ["phys 121", "phys 122"]]}
     reply = ""
-
-    if user['COR'] in direct[user['MAJOR']]:
-        reply = "Yes " + user['COR'].upper() + " is a requirement for " + user['MAJOR'].upper() + "." 
+    
+    if user["MAJOR"] in list(direct.keys()):
+        if user['COR'] in direct[user['MAJOR']]:
+            reply = "Yes " + user['COR'].upper() + " is a requirement for " + user['MAJOR'].upper() + "." 
+        else:
+            for type in optional[user['MAJOR']]:
+                if isinstance(type, dict):
+                    l=[]
+                    [l.extend([k,v]) for k,v in type.items()]
+                    y = [l[0]]
+                    [y.extend(l[1])]
+                    if user['COR'] in y:
+                        reply = "Yes, take one of "+ y[0].upper() + " or " + y[1] + " of "  
+                        del y[0]
+                        del y[0]
+                        for i in y:
+                            reply += i.upper() + ", "
+                else:
+                    if user['COR'] in type:
+                        reply = "Yes, take one of "+ type[0].upper() 
+                        del type[0]
+                        for i in type:
+                            reply += " or " + i.upper()
     else:
-        for type in optional[user['MAJOR']]:
-            if isinstance(type, dict):
-                l=[]
-                [l.extend([k,v]) for k,v in type.items()]
-                y = [l[0]]
-                [y.extend(l[1])]
-                if user['COR'] in y:
-                    reply = "Yes, take one of "+ y[0].upper() + " or " + y[1] + " of "  
-                    del y[0]
-                    del y[0]
-                    for i in y:
-                        reply += i.upper() + ", "
-            else:
-                if user['COR'] in type:
-                    reply = "Yes, take one of "+ type[0].upper() 
-                    del type[0]
-                    for i in type:
-                        reply += " or " + i.upper()  
-        
+        reply = "Major information not available."
+            
     if reply == "":
         reply = user['COR'].upper() + " is not a requirement for " + user['MAJOR'] + " but might be used as an elective, speak with an Academic & Career Advisor for more clarity."
 
